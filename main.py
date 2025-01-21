6355aad5b0fc9b1625ec1a2eab698d3e9b88d19b
Zainab Aamir <zaamir427@gmail.com>
2025-01-21 23:43:27 +0500

    added new information

--- main.py
+++ main.py
@@ -3,9 +3,27 @@ import requests
 
 response = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=')
 soup = BeautifulSoup(response.content, 'lxml')
-job = soup.find('li', class_ ="clearfix job-bx wht-shd-bx")
-companyName = job.find('h2', class_ = 'heading-trun').text.replace(' ', '')
-print(companyName)
-skills = job.find('li', class_ = 'job-description__').text
-print(skills)
+jobs = soup.find_all('li', class_ ="clearfix job-bx wht-shd-bx")
+for job in jobs:
+    publishedDate = job.find('span', class_='sim-posted').span.text
+    if 'few' in publishedDate:
+        companyName = job.find('h3', class_ = 'joblist-comp-name').text.strip()
+        skills = job.find('div', class_ = 'more-skills-sections').text.strip()
+        
 
+
+
+
+        print(f'''
+        Company Name : {companyName}
+        Required Skills : {skills}
+        ''')
+        
+        print('')
+
+
+
+
+
+
+ 
